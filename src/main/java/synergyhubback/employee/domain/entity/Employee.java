75482738cc3486c827fe4b3b9ba2c610668d9c00
SILVER-BE <dhfldmlwkrdmsdusaht@gmail.com>
2024-06-14 16:27:04 +0900

    #6 merge 수정

--- src/main/java/synergyhubback/employee/domain/entity/Employee.java
+++ src/main/java/synergyhubback/employee/domain/entity/Employee.java
@@ -2,23 +2,11 @@ package synergyhubback.employee.domain.entity;
 
 
 import jakarta.persistence.*;
-<<<<<<< HEAD
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import synergyhubback.attendance.domain.entity.Attendance;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-=======
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
->>>>>>> db304118d6f8d2df3af4aa2f16016264e60ca887
 
 @Entity
 @Table(name = "employee_info")
@@ -30,11 +18,7 @@ public class Employee {
     @Id
     private int emp_code;
     private String emp_name;
-<<<<<<< HEAD
-    private int emp_pass;
-=======
     private String emp_pass;
->>>>>>> db304118d6f8d2df3af4aa2f16016264e60ca887
     private String social_security_no;
     private String email;
     private String phone;
@@ -46,14 +30,6 @@ public class Employee {
     private String emp_status;
     private String emp_sign;
     private String emp_img;
-<<<<<<< HEAD
-    private String dept_code;
-    private String title_code;
-    private String position_code;
-    private int bank_code;
-
-    public Employee(int emp_code, String emp_name, int emp_pass, String social_security_no, LocalDate hire_date, String emp_status, String dept_code, String title_code, String position_code) {
-=======
 //    private String dept_code;
 //    private String title_code;
 //    private String position_code;
@@ -74,20 +50,12 @@ public class Employee {
     private int bank_code;
 
     public Employee(int emp_code, String emp_name, String emp_pass, String social_security_no, LocalDate hire_date, String emp_status, String dept_code, String title_code, String position_code) {
->>>>>>> db304118d6f8d2df3af4aa2f16016264e60ca887
         this.emp_code = emp_code;
         this.emp_name = emp_name;
         this.emp_pass = emp_pass;
         this.social_security_no = social_security_no;
         this.hire_date = hire_date;
         this.emp_status = emp_status;
-<<<<<<< HEAD
-        this.dept_code = dept_code;
-        this.title_code = title_code;
-        this.position_code = position_code;
-    }
-
-=======
 //        this.dept_code = dept_code;
 //        this.title_code = title_code;
 //        this.position_code = position_code;
@@ -107,5 +75,4 @@ public class Employee {
                 position_code
         );
     }
->>>>>>> db304118d6f8d2df3af4aa2f16016264e60ca887
 }
