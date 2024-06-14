de02e598b89f63a37ff3b5a5210d4fba2633b94d
JH5256 <skdltmvhs2@naver.com>
2024-06-14 18:30:53 +0900

    #3 merge update

--- src/main/java/synergyhubback/calendar/domain/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package synergyhubback.calendar.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import synergyhubback.calendar.domain.entity.Employee;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
