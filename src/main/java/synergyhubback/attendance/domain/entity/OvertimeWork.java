f06149c0d958f35f5e46adafc36350087ce2a34b
won1999won <won1999won6@gmail.com>
2024-06-18 10:08:57 +0900

    Merge branch 'main' into Post/kjw

--- src/main/java/synergyhubback/attendance/domain/entity/OvertimeWork.java
+++ src/main/java/synergyhubback/attendance/domain/entity/OvertimeWork.java
@@ -8,18 +8,18 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalTime;
 
-//@Entity
-////@Table(name = "overtime_work")
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
-//@Getter
-//@EntityListeners(AuditingEntityListener.class)
-//public class OvertimeWork {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int owCode;            //초과근무코드(pk)
-//    private LocalTime startTime;    //시작시간
-//    private LocalTime endTime;      //종료시간
-//    private int empCode;           //사원코드 (추후 fk)
-//
-//}
+@Entity
+@Table(name = "overtime_work")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class OvertimeWork {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int owCode;            //초과근무코드(pk)
+    private LocalTime startTime;    //시작시간
+    private LocalTime endTime;      //종료시간
+    private int empCode;           //사원코드 (추후 fk)
+
+}
--- src/main/java/synergyhubback/calendar/domain/repository/EmployeeRepository.java
+++ src/main/java/synergyhubback/calendar/domain/repository/EmployeeRepository.java
@@ -2,8 +2,8 @@ package synergyhubback.calendar.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import synergyhubback.calendar.domain.entity.Event;
+import synergyhubback.calendar.domain.entity.Employee;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
