afce120a7a16b0651bb350a86e643939b33dc69e
jinyoung23456 <jinyoung39024@gmail.com>
2024-06-19 17:11:49 +0900

    #16 이벤트 파트 업데이트

--- src/main/java/synergyhubback/calendar/dto/request/EventCreateRequest.java
+++ src/main/java/synergyhubback/calendar/dto/request/EventCreateRequest.java
@@ -1,14 +1,17 @@
 package synergyhubback.calendar.dto.request;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@RequiredArgsConstructor
+@Setter
 public class EventCreateRequest {
 
     @NotBlank
@@ -18,8 +21,10 @@ public class EventCreateRequest {
     private final String eventCon;
 
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private final LocalDateTime startDate;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private final LocalDateTime endDate;
 
     private final boolean allDay;
@@ -27,8 +32,28 @@ public class EventCreateRequest {
     private final String eventGuests;
 
     @NotNull
-    private final Integer empCode; // 사원 코드
+    private final int empCode; // 사원 코드
 
     @NotNull
     private final Long labelCode; // 라벨 코드
+
+    @JsonCreator
+    public EventCreateRequest(
+            @JsonProperty("title") String title,
+            @JsonProperty("eventCon") String eventCon,
+            @JsonProperty("startDate") LocalDateTime startDate,
+            @JsonProperty("endDate") LocalDateTime endDate,
+            @JsonProperty("allDay") boolean allDay,
+            @JsonProperty("eventGuests") String eventGuests,
+            @JsonProperty("empCode") int empCode,
+            @JsonProperty("labelCode") Long labelCode) {
+        this.title = title;
+        this.eventCon = eventCon;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.allDay = allDay;
+        this.eventGuests = eventGuests;
+        this.empCode = empCode;
+        this.labelCode = labelCode;
+    }
 }
