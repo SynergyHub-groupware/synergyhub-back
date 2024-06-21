3215b082e16b77add5412bcdcb9d8bf9404217e6
LXXDJ <joker96.dj@gmail.com>
2024-06-21 18:19:11 +0900

    mid commit

--- src/main/java/synergyhubback/approval/domain/repository/TrueLineRepository.java
+++ src/main/java/synergyhubback/approval/domain/repository/TrueLineRepository.java
@@ -26,19 +26,26 @@ public interface TrueLineRepository extends JpaRepository<TrueLine, Integer> {
             "SUBSTRING(d.adCode, 1, 2), CAST(SUBSTRING(d.adCode, 3) AS Integer) DESC")
     List<TrueLine> findTrueLineWithPendingStatus(Integer empCode, String status);
 
+//    @Query("SELECT t FROM TrueLine t " +
+//            "JOIN t.document d " +
+//            "JOIN d.employee e " +
+//            "JOIN d.form f " +
+//            "WHERE t.talOrder = (" +
+//            "    SELECT MIN(t2.talOrder) " +
+//            "    FROM TrueLine t2 " +
+//            "    WHERE t2.document = d " +
+//            "    AND t2.talStatus = '미결재' " +
+//            ")" +
+//            "AND d.adStatus = :status " +
+//            "AND d.employee.emp_code = :empCode " +
+//            "ORDER BY d.adReportDate DESC, " +
+//            "SUBSTRING(d.adCode, 1, 2), CAST(SUBSTRING(d.adCode, 3) AS Integer) DESC")
+//    Page<TrueLine> findTrueLineWithPendingStatus(Pageable pageable, Integer empCode, String status);
+
     @Query("SELECT t FROM TrueLine t " +
-            "JOIN t.document d " +
-            "JOIN d.employee e " +
-            "JOIN d.form f " +
-            "WHERE t.talOrder = (" +
-            "    SELECT MIN(t2.talOrder) " +
-            "    FROM TrueLine t2 " +
-            "    WHERE t2.document = d " +
-            "    AND t2.talStatus = '미결재' " +
-            ")" +
-            "AND d.adStatus = :status " +
-            "AND d.employee.emp_code = :empCode " +
-            "ORDER BY d.adReportDate DESC, " +
-            "SUBSTRING(d.adCode, 1, 2), CAST(SUBSTRING(d.adCode, 3) AS Integer) DESC")
-    Page<TrueLine> findTrueLineWithPendingStatus(Pageable pageable, Integer empCode, String status);
+            "JOIN t.employee e " +
+            "JOIN e.department d " +
+            "JOIN e.title tt " +
+            "WHERE t.document.adCode = :adCode")
+    List<TrueLine> findViewLineList(String adCode);
 }
---
+++ src/main/java/synergyhubback/approval/dto/response/ViewLineResponse.java
@@ -0,0 +1,36 @@
+package synergyhubback.approval.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import synergyhubback.approval.domain.entity.TrueLine;
+
+import java.time.LocalDate;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ViewLineResponse {
+    private final int talOrder;
+    private final String talRole;
+    private final String talStatus;
+    private final LocalDate talDate;
+    private final String deptTitle;
+    private final String titleName;
+    private final String empName;
+    private final String empSign;
+    private final String talReason;
+
+    public static ViewLineResponse from(final TrueLine trueLine){
+        return new ViewLineResponse(
+                trueLine.getTalOrder(),
+                trueLine.getTalRole(),
+                trueLine.getTalStatus(),
+                trueLine.getTalDate(),
+                trueLine.getEmployee().getDepartment().getDept_title(),
+                trueLine.getEmployee().getTitle().getTitle_name(),
+                trueLine.getEmployee().getEmp_name(),
+                trueLine.getEmployee().getEmp_sign(),
+                trueLine.getTalReason()
+        );
+    }
+}
